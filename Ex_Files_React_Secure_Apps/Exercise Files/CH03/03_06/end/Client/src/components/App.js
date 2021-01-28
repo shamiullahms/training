07ad6a539ca61d0d8f80c3820fcfa98305f6d994
Shamiullah Mustafa <Shamiullah.Mustafa@gaf.com>
2021-01-27 22:44:02 -0500

    First Load all Training Apps

---
+++ Ex_Files_React_Secure_Apps/Exercise Files/CH03/03_06/end/Client/src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth0 } from '../react-auth0-spa';
+import history from '../history';
+import Navigation from './Navigation';
+import Jumbotron from './Jumbotron';
+import Feed from './Feed';
+import Contact from './Contact';
+import About from './About';
+import Loading from './Loading';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      name: "Manny Henri",
+      jumbotronTitle: "List of courses",
+      feeds: [],
+    }
+  }
+
+  async componentDidMount() {
+    const url = 'http://localhost:4000/courses';
+    const response = await axios.get(url);
+    return this.setState({ feeds: response.data })
+  }
+
+  render() {
+    const { loading } = useAuth0;
+
+    if (loading) {
+      return <Loading />
+    }
+
+    return (
+      <Router history={history}>
+        <div className="container">
+          <Navigation />
+          <Jumbotron title={this.state.jumbotronTitle}/>
+          <Switch>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/about" component={About}/>
+            <Route exact path="/" render={(props) => (
+              <Feed feeds={this.state.feeds} />
+            )} />
+          </Switch>
+          <div className="footer">
+                <p>&copy; {this.state.name} Inc.</p>
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
